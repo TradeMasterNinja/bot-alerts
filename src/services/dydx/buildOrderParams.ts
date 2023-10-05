a3ccf5eb5d936ae8c30cf2c8046ebc6260d754cd
Ray Meeks <Ray@RayMeeks.com>
2023-10-05 03:39:31 -0600

    Update buildOrderParams.ts
    remove ugly if else if else

--- src/services/dydx/buildOrderParams.ts
+++ src/services/dydx/buildOrderParams.ts
@@ -13,7 +13,6 @@ import { getDecimalPointLength, getStrategiesDB } from '../../helper';
 export const dydxBuildOrderParams = async (alertMessage: AlertObject) => {
     const [db, rootData] = getStrategiesDB();
 
-    // set expiration datetime. must be more than 1 minute from the current datetime
     const currentDate: Date = new Date();
     const futureDate: Date =
         alertMessage.type === 'market'
@@ -27,71 +26,43 @@ export const dydxBuildOrderParams = async (alertMessage: AlertObject) => {
     const market = Market[alertMessage.market as keyof typeof Market];
     const marketsData = await connector.client.public.getMarkets(market);
 
-    const orderSide =
+    const orderSide: OrderSide =
         alertMessage.order == 'buy' ? OrderSide.BUY : OrderSide.SELL;
 
     const latestPrice = parseFloat(marketsData.markets[market].oraclePrice);
     console.log('latestPrice', latestPrice);
 
-    let orderSize: number;
-    if (alertMessage.sizeByLeverage) {
-        const account = await connector.client.private.getAccount(
-            process.env.ETH_ADDRESS
-        );
-        const equity = Number(account.account.equity);
-        orderSize = (equity * Number(alertMessage.sizeByLeverage)) / latestPrice;
-    } else if (alertMessage.sizeUsd) {
-        orderSize = Number(alertMessage.sizeUsd) / latestPrice;
-    } else if (
-        alertMessage.reverse &&
-        rootData[alertMessage.strategy].isFirstOrder == 'false'
-    ) {
-        orderSize = alertMessage.size * 2;
-    } else {
-        orderSize = alertMessage.size;
-    }
+    const orderSize: number = 
+        alertMessage.sizeByLeverage
+            ? (Number((await connector.client.private.getAccount(process.env.ETH_ADDRESS)).account.equity) * Number(alertMessage.sizeByLeverage)) / latestPrice
+            : alertMessage.sizeUsd
+            ? Number(alertMessage.sizeUsd) / latestPrice
+            : alertMessage.reverse && rootData[alertMessage.strategy].isFirstOrder === 'false'
+            ? alertMessage.size * 2
+            : alertMessage.size;
 
-    let orderType: OrderType;
-    let price1: string;
-    const trailingpercent =
-        alertMessage.trailingPercent !== undefined
-            ? alertMessage.trailingPercent
-            : null;
 
-    if (alertMessage.type === 'market') {
-        orderType = OrderType.MARKET;
-        price1 = latestPrice.toFixed(getDecimalPointLength(latestPrice)).toString();
-    } else if (alertMessage.type === 'take profit') {
-        orderType = OrderType.TAKE_PROFIT;
-        price1 = Number(alertMessage.enterPrice)
-            .toFixed(getDecimalPointLength(latestPrice))
-            .toString();
-    } else if (alertMessage.type === 'limit') {
-        orderType = OrderType.LIMIT;
-        price1 = Number(alertMessage.enterPrice)
-            .toFixed(getDecimalPointLength(latestPrice))
-            .toString();
-    } else if (alertMessage.type === 'trailing stop') {
-        orderType = OrderType.TRAILING_STOP;
-        price1 = Number(alertMessage.enterPrice)
-            .toFixed(getDecimalPointLength(latestPrice))
-            .toString();
-        //if (alertMessage.order == 'buy')
-        //{
-        //const trailingAmount = latestPrice * ((alertMessage.trailingPercent) / 100);
-        //price1 = (latestPrice + trailingAmount).toFixed(getDecimalPointLength(latestPrice)).toString();}
-        /*else {
-                const trailingAmount = latestPrice * (-(alertMessage.trailingPercent / 100));
-                price = (latestPrice + trailingAmount).toFixed(getDecimalPointLength(latestPrice)).toString();
-            }*/
-    } else {
-        orderType = OrderType.MARKET;
-        price1 = latestPrice.toFixed(getDecimalPointLength(latestPrice)).toString();
-    }
+
+    const orderType: OrderType =
+        alertMessage.type === 'market'
+            ? OrderType.MARKET
+            : alertMessage.type === 'take profit'
+            ? OrderType.TAKE_PROFIT
+            : alertMessage.type === 'limit'
+            ? OrderType.LIMIT
+            : alertMessage.type === 'trailing stop'
+            ? OrderType.TRAILING_STOP
+            : OrderType.MARKET;
+
+    const price1: string =
+        orderType === OrderType.MARKET || orderType === OrderType.LIMIT || orderType === OrderType.TRAILING_STOP
+            ? Number(alertMessage.enterPrice).toFixed(getDecimalPointLength(latestPrice)).toString()
+            : latestPrice.toFixed(getDecimalPointLength(latestPrice)).toString();
 
     const stepSize = parseFloat(marketsData.markets[market].stepSize);
     const stepDecimal = getDecimalPointLength(stepSize);
     const orderSizeStr = Number(orderSize).toFixed(stepDecimal);
+
     const price2 = parseFloat(price1);
     const tickSize = parseFloat(marketsData.markets[market].tickSize);
     const roundedPrice = Math.round(price2 / tickSize) * tickSize;
@@ -99,31 +70,25 @@ export const dydxBuildOrderParams = async (alertMessage: AlertObject) => {
     const price3 = roundedPrice.toFixed(getDecimalPointLength(tickSize)).toString();
 
     const slippagePercentage = 0.05;
-    if (alertMessage.type == 'market') {
-        const minPrice =
-            orderSide == OrderSide.BUY
+    const minPrice =
+        alertMessage.type === 'market'
+            ? orderSide === OrderSide.BUY
                 ? roundedPrice * (1 + slippagePercentage)
-                : roundedPrice * (1 - slippagePercentage);
+                : roundedPrice * (1 - slippagePercentage)
+            : price3;
 
-        const decimal = getDecimalPointLength(tickSize);
-        const price3 = minPrice.toFixed(decimal);
-    }
+    const decimal = getDecimalPointLength(tickSize);
+    const price4 = minPrice.toFixed(decimal);
 
-    let time1 = null;
-    if (orderType == OrderType.LIMIT) {
-        time1 = TimeInForce.GTT;
-    } else if (orderType == OrderType.MARKET) {
-        time1 = TimeInForce.FOK;
-    } else {
-        time1 = TimeInForce.IOC;
-    }
+    const time1 =
+        orderType === OrderType.LIMIT
+            ? TimeInForce.GTT
+            : orderType === OrderType.MARKET
+            ? TimeInForce.FOK
+            : TimeInForce.IOC;
 
-    let reduceonly = false;
-    if (orderType == OrderType.MARKET) {
-        reduceonly = false;
-    } else {
-        reduceonly = alertMessage.reduceOnly;
-    }
+    const reduceonly: boolean =
+        orderType === OrderType.MARKET ? false : alertMessage.reduceOnly || false;
 
     const orderParams: dydxOrderParams = {
         market: market,
@@ -131,19 +96,18 @@ export const dydxBuildOrderParams = async (alertMessage: AlertObject) => {
         timeInForce: time1,
         postOnly: false,
         size: orderSizeStr,
-        price: price3,
+        price: price4,
         limitFee: config.get('Dydx.User.limitFee'),
         expiration: dateStr,
         type: orderType,
         reduceOnly: reduceonly,
     };
+
     if (trailingpercent !== null) {
-         // Check if orderSide is "sell" and convert trailingpercent to a negative value
         orderParams.trailingPercent = orderSide === OrderSide.SELL ? -trailingpercent : trailingpercent;
-        orderParams.triggerPrice = price3;
-    }
-    if (trailingpercent == null && orderType == OrderType.TAKE_PROFIT) {
-        orderParams.triggerPrice = price3;
+        orderParams.triggerPrice = price4;
+    } else if (trailingpercent == null && orderType == OrderType.TAKE_PROFIT) {
+        orderParams.triggerPrice = price4;
     }
 
     console.log('orderParams for dydx', orderParams);
