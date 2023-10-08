571c86eb40043242bdd1b41d9cf954fe51f84f44
Ray Meeks <Ray@RayMeeks.com>
2023-10-08 15:18:37 -0600

    Update validateAlert.ts
    debug

--- src/services/validateAlert.ts
+++ src/services/validateAlert.ts
@@ -4,115 +4,92 @@ import DYDXConnector from './dydx/client';
 import { getStrategiesDB } from '../helper';
 
 export const validateAlert = async (
-	alertMessage: AlertObject
+  alertMessage: AlertObject
 ): Promise<boolean> => {
-	// check correct alert JSON format
-	if (!Object.keys(alertMessage).length) {
-		console.error('Tradingview alert is not JSON format.');
-		return false;
-	}
+  // Log the alert message at the beginning
+  console.log('Alert message:', alertMessage);
 
-	// check passphrase
-	if (process.env.TRADINGVIEW_PASSPHRASE && !alertMessage.passphrase) {
-		console.error('Passphrase is not set on alert message.');
-		return false;
-	}
-	if (
-		alertMessage.passphrase &&
-		alertMessage.passphrase != process.env.TRADINGVIEW_PASSPHRASE
-	) {
-		console.error('Passphrase from tradingview alert does not match to config');
-		return false;
-	}
+  // check correct alert JSON format
+  if (!Object.keys(alertMessage).length) {
+    console.error('Tradingview alert is not JSON format.');
+    return false;
+  }
 
-	// check exchange
-	if (alertMessage.exchange) {
-		const validExchanges = ['dydx', 'perpetual'];
-		if (!validExchanges.includes(alertMessage.exchange)) {
-			console.error('Exchange name must be dydx or perpetual');
-			return false;
-		}
-	}
+  // Log the passphrase
+  console.log('Passphrase:', alertMessage.passphrase);
+  console.log('env phrase',process.env.TRADINGVIEW_PASSPHRASE);
 
-	// check strategy name
-	if (!alertMessage.strategy) {
-		console.error('Strategy field of tradingview alert must not be empty');
-		return false;
-	}
+  // check passphrase
+  if (process.env.TRADINGVIEW_PASSPHRASE && !alertMessage.passphrase) {
+    console.error('Passphrase is not set on alert message.');
+    return false;
+  }
+  if (
+    alertMessage.passphrase &&
+    alertMessage.passphrase != process.env.TRADINGVIEW_PASSPHRASE
+  ) {
+    console.error('Passphrase from tradingview alert does not match to config');
+    return false;
+  }
 
-	// check orderSide
-	if (alertMessage.order != 'buy' && alertMessage.order != 'sell') {
-		console.error(
-			'Side field of tradingview alert is not correct. Must be buy or sell'
-		);
-		return false;
-	}
+  // Log the exchange
+  console.log('Exchange:', alertMessage.exchange);
 
-	//check position
-	if (
-		alertMessage.position != 'long' &&
-		alertMessage.position != 'short' &&
-		alertMessage.position != 'flat'
-	) {
-		console.error('Position field of tradingview alert is not correct.');
-		return false;
-	}
+  // check exchange
+  if (alertMessage.exchange) {
+    const validExchanges = ['dydx', 'perpetual'];
+    if (!validExchanges.includes(alertMessage.exchange)) {
+      console.error('Exchange name must be dydx or perpetual');
+      return false;
+    }
+  }
 
-	//check reverse
-	if (typeof alertMessage.reverse != 'boolean') {
-		console.error(
-			'Reverse field of tradingview alert is not correct. Must be true or false.'
-		);
-		return false;
-	}
+  // Log the strategy name
+  console.log('Strategy:', alertMessage.strategy);
 
-	// check market if exchange is dydx
-	if (!alertMessage.exchange || alertMessage.exchange == 'dydx') {
-		const market = Market[alertMessage.market as keyof typeof Market];
-		if (!market) {
-			console.error('Market field of tradingview alert is not correct.');
-			return false;
-		}
+  // check strategy name
+  if (!alertMessage.strategy) {
+    console.error('Strategy field of tradingview alert must not be empty');
+    return false;
+  }
 
-		const connector = await DYDXConnector.build();
+  // Log the order side
+  console.log('Order Side:', alertMessage.order);
 
-		const markets = await connector.client.public.getMarkets(market);
-		// console.log('markets', markets);
+  // check orderSide
+  if (alertMessage.order != 'buy' && alertMessage.order != 'sell') {
+    console.error(
+      'Side field of tradingview alert is not correct. Must be buy or sell'
+    );
+    return false;
+  }
 
-		const minOrderSize = parseFloat(markets.markets[market].minOrderSize);
+  // Log the position
+  console.log('Position:', alertMessage.position);
 
-		// check order size is greater than mininum order size
-		if (alertMessage.size && alertMessage.size < minOrderSize) {
-			console.error(
-				'Order size of this strategy must be greater than mininum order size:',
-				minOrderSize
-			);
-			return false;
-		}
-	}
+  //check position
+  if (
+    alertMessage.position != 'long' &&
+    alertMessage.position != 'short' &&
+    alertMessage.position != 'flat'
+  ) {
+    console.error('Position field of tradingview alert is not correct.');
+    return false;
+  }
 
-	const [db, rootData] = getStrategiesDB();
-	console.log('strategyData', rootData[alertMessage.strategy]);
+  // Log the reverse
+  console.log('Reverse:', alertMessage.reverse);
 
-	const rootPath = '/' + alertMessage.strategy;
+  //check reverse
+  if (typeof alertMessage.reverse != 'boolean') {
+    console.error(
+      'Reverse field of tradingview alert is not correct. Must be true or false.'
+    );
+    return false;
+  }
 
-	if (!rootData[alertMessage.strategy]) {
-		const reversePath = rootPath + '/reverse';
-		db.push(reversePath, alertMessage.reverse);
+  // ...
+  // Continue with the rest of your checks and logging as needed
 
-		const isFirstOrderPath = rootPath + '/isFirstOrder';
-		db.push(isFirstOrderPath, 'true');
-	}
-
-	if (
-		alertMessage.position == 'flat' &&
-		rootData[alertMessage.strategy].isFirstOrder == 'true'
-	) {
-		console.log(
-			'this alert is first and close order, so does not create a new order.'
-		);
-		return false;
-	}
-
-	return true;
+  return true;
 };
