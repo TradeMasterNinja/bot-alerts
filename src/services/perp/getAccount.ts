ba184ade58b0dd22dad52edf944355da2b18af17
Ray Meeks <Ray@RayMeeks.com>
2023-10-08 17:35:54 -0600

    Update getAccount.ts

--- src/services/perp/getAccount.ts
+++ src/services/perp/getAccount.ts
@@ -1,19 +1,20 @@
 import PerpetualConnector from './client';
 
 export const perpGetAccount = async () => {
-	try {
-		const perp = await PerpetualConnector.build();
-		if (!perp || !perp.wallet) return false;
+  try {
+    const perp = await PerpetualConnector.build();
+    if (!perp || !perp.wallet) return { account: null, success: false };
 
-		const account = await perp.wallet.getBalanceEth();
-		console.log('Perpetual Protocol ETH balance: ', Number(account));
+    const account = await perp.wallet.getBalanceEth();
+    console.log('Perpetual Protocol ETH balance: ', Number(account));
 
-		if (Number(account) == 0) {
-			return false;
-		} else {
-			return true;
-		}
-	} catch (error) {
-		console.error(error);
-	}
+    if (Number(account) === 0) {
+      return { account, success: false };
+    } else {
+      return { account, success: true };
+    }
+  } catch (error) {
+    console.error(error);
+    return { account: null, success: false };
+  }
 };
