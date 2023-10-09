7b2cb322256fee8258e631ef3c6969ff93fdf212
Ray Meeks <Ray@RayMeeks.com>
2023-10-08 22:52:13 -0600

    Update checkAfterPosition.ts
    ..

--- src/services/checkAfterPosition.ts
+++ src/services/checkAfterPosition.ts
@@ -1,21 +1,37 @@
 import { getStrategiesDB } from '../helper';
 import { AlertObject } from '../types';
 
-export const checkAfterPosition = async (alertMessage: AlertObject) => {
-	const [db, rootData] = getStrategiesDB();
+export const checkAfterPosition = async (
+  alertMessage: AlertObject
+): Promise<number | null> => {
+  const [db, rootData] = getStrategiesDB();
 
-	const storedPositionSize = rootData[alertMessage.strategy].position;
-	// console.log('storedPositionSize', storedPositionSize);
-	let storedPosition;
-	if (storedPositionSize > 0) {
-		storedPosition = 'long';
-	} else if (storedPositionSize < 0) {
-		storedPosition = 'short';
-	} else {
-		storedPosition = 'flat';
-	}
+  const storedPositionSize = rootData[alertMessage.strategy].position;
 
-	if (storedPosition != alertMessage.position) {
-		console.error('dYdX position does not match to Tradingview Position.');
-	}
+  // Calculate the absolute value of the position size
+  const absPositionSize = Math.abs(storedPositionSize);
+
+  return absPositionSize;
 };
+
+
+
+
+// export const checkAfterPosition = async (alertMessage: AlertObject) => {
+// 	const [db, rootData] = getStrategiesDB();
+
+// 	const storedPositionSize = rootData[alertMessage.strategy].position;
+// 	// console.log('storedPositionSize', storedPositionSize);
+// 	let storedPosition;
+// 	if (storedPositionSize > 0) {
+// 		storedPosition = 'long';
+// 	} else if (storedPositionSize < 0) {
+// 		storedPosition = 'short';
+// 	} else {
+// 		storedPosition = 'flat';
+// 	}
+
+// 	if (storedPosition != alertMessage.position) {
+// 		console.error('dYdX position does not match to Tradingview Position.');
+// 	}
+// };
