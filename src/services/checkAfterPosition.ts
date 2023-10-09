60587ae7da5e28faa66d0b7ff48b06a71c48bb78
Ray Meeks <Ray@RayMeeks.com>
2023-10-09 00:33:47 -0600

    Update checkAfterPosition.ts

--- src/services/checkAfterPosition.ts
+++ src/services/checkAfterPosition.ts
@@ -3,40 +3,12 @@ import { AlertObject } from '../types';
 
 export const checkAfterPosition = async (
   alertMessage: AlertObject
-): Promise<number | null> => {
+): Promise<any | null> => {
   const [db, rootData] = getStrategiesDB();
 
   if (!rootData[alertMessage.strategy] || !rootData[alertMessage.strategy].position) {
     return null;
   }
+
   return rootData;
-  //const storedPositionSize = rootData[alertMessage.strategy].position;
-
-  // Calculate the absolute value of the position size
-  //const absPositionSize = Math.abs(storedPositionSize);
-
-  //return absPositionSize;
 };
-
-
-
-
-
-// export const checkAfterPosition = async (alertMessage: AlertObject) => {
-// 	const [db, rootData] = getStrategiesDB();
-
-// 	const storedPositionSize = rootData[alertMessage.strategy].position;
-// 	// console.log('storedPositionSize', storedPositionSize);
-// 	let storedPosition;
-// 	if (storedPositionSize > 0) {
-// 		storedPosition = 'long';
-// 	} else if (storedPositionSize < 0) {
-// 		storedPosition = 'short';
-// 	} else {
-// 		storedPosition = 'flat';
-// 	}
-
-// 	if (storedPosition != alertMessage.position) {
-// 		console.error('dYdX position does not match to Tradingview Position.');
-// 	}
-// };
