6ef0a3e0dce015589d9a947efb9a229831dd8178
Ray Meeks <Ray@RayMeeks.com>
2023-10-09 00:36:38 -0600

    Update resetDB.ts

--- src/services/resetDB.ts
+++ src/services/resetDB.ts
@@ -1,3 +1,10 @@
 import { resetDB } from '../helper';
 import { AlertObject } from '../types';
 
+export const checkAfterPosition = async (
+  alertMessage: AlertObject
+): Promise<any | null> => {
+  if (alertMessage.resetDB){
+    await resetDB();
+  }
+};
